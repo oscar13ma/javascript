7513999cb59a22e4694c11fd04de9f3748f9ca82
Oscar Mamani <omamani58@gmail.com>
2023-10-29 12:39:53 -0300

    tipos de datos js

--- main.js
+++ main.js
@@ -1,1 +1,79 @@
-alert("hola mundo") ;
+// alert("hola mundo") ;
+//     let nombre = "Ignacio" ;
+//     var apellido = "Mamani" ;
+
+
+//  const PI = 3.14;
+
+// nombre = "oscar"
+
+
+//     console.log("Nombre : ",nombre);
+//     console.log("Apellido : ",apellido) ;
+    
+
+
+    // console.log("Apellido Global: ",apellido);
+
+// function main() {
+//     let nombre = "Ignacio" ;
+//     var apellido = "Mamani" ;
+
+
+//     console.log("Nombre Local: ",nombre);
+//     console.log("Apellido Local: ",apellido, );
+// }
+// main()
+// nombre = "Oscar"
+// apellido = "Barrera Oro" ;
+
+// console.log("Apellido Global: ",apellido) ;
+// console.log("Nombre Global: ",nombre);
+
+
+// if (true) {
+
+//     tambien se lo uso como el function main 
+// }
+
+// DATOS PRIMITIVOS
+
+// string cadena de texto
+let vendedor = "enrico" ;
+let vendedor2 = 'gonzalo' ;
+console.log("vendedor: ", typeof vendedor) 
+
+let number1 = 31 ;
+
+let number2 = 13.05 ;
+console.log("Number1: ", typeof number1) 
+console.log("Number2: ", typeof number2)
+
+// boleans
+let verdadero = true ;
+let falso = false ;
+console.log("Verdadero: ", typeof verdadero)
+console.log("Falso: ", typeof falso)
+// nulo
+let dinero = null ;
+console.log("Dinero: ", typeof dinero)
+// undefined
+ let sueldo; 
+ console.log("Sueldo: ", typeof sueldo)
+
+//  DATOS COMPLEJOS
+
+// Array arreglo
+let arreglo = [1, "Oscar", true]
+
+
+
+// object objeto
+let persona = {
+ nombre: "Oscar",
+ apellido: "Mamani",
+ edad: 31,
+ ciudadNatal: "Bda rio sali", 
+
+}
+// (va la coma aqui o no) en ciudad natal
