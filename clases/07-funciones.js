14a46bc335877fd0c21981d325cb55cbc39ed81b
Oscar Mamani <omamani58@gmail.com>
2023-11-18 20:04:24 -0300

    clase07-funciones

---
+++ clases/07-funciones.js
@@ -0,0 +1,43 @@
+
+const notas1 = [5, 7, 9, 10];
+const notas2  = [2, 4, 7, 9]
+
+
+//funcion declarativa
+function promediar(notas) {
+    
+
+
+let sumatoria = 0
+
+for (let index = 0; index < notas.length; index++) {
+    //sumatoria=sumatoria+numeros
+    sumatoria += notas[index];
+}
+    let promedio = sumatoria / notas.length;
+
+    return promedio; 
+
+
+}
+
+
+
+let promedio1 = promediar(notas1);
+let promedio2 = promediar(notas2);
+
+console.log(promedio1);
+console.log(promedio2);
+
+
+//funcion expresiva/funcion anonima
+const mostrarPromedio = function(notas, callback) {
+console.log(`las notas son: ${notas}`)
+let promedio = callback(notas)
+
+console.log(`el promedio de las notas en todo el año es: ${promedio}`)
+}
+mostrarPromedio(notas1, promediar);
+mostrarPromedio(notas2, promediar); 
+
+//callbacks
--- ejercicios/ejercicios.js
+++ ejercicios/ejercicios.js
@@ -1,46 +1,67 @@
 //recibir dos valores y operaciones a realizar  buscar!!!!
 
 
-let num1 = Number(prompt('ingrese el primer factor'))
+
+
+// console.log(num1)
+// console.log(num2)
+
+
+
+
+function calculadora(factor1, factor2, operacion ) {
+    if (isNaN(factor1)) {
+        
+    return alert(`el factor1 ${factor1}, no es un numero`)
+    
+    }
+    
+    if (isNaN(factor2))  {
+    
+       
+        return alert(`el factor2 ${factor2}, no es un numero`)
+    }
+    
+    // alert(factor1)
+    // alert(factor2)
+    // alert(operacion)
+    
+    switch (operacion) {
+        case '+':
+         case 'suma':   
+            alert(`El resultado de ${factor1} + ${factor2} = ${factor1 + factor2}`); 
+            
+            break;
+            case '-':
+                case'resta':
+            alert(`El resultado de  ${factor1} - ${factor2} = ${factor1 - factor2}`)
+            break;
+            case '*': 
+            case 'multiplicacion':
+            alert(`El resultado de  ${factor1} * ${factor2} = ${factor1 * factor2}`)
+            break;
+            case '/':
+                case 'division': 
+            alert(`El resultado de  ${factor1} / ${factor2} = ${factor1 / factor2}`)
+            break; 
+        default:
+    
+            console.error(`La operacion ${operacion} no existe en esta calculadora`)
+            break;
+    }
+    
+}
+
+let confirmacion;
+
+do {
+    let num1 = Number(prompt('ingrese el primer factor'))
 let num2 = Number(prompt('ingrese el segundo  factor'))
 let operacion = prompt('ingrese la operacion a realizar')
 
-console.log(num1)
-console.log(num2)
 
+calculadora(num1, num2, operacion);
 
-if (isNaN(num1)) {
-    num1 = 0;
+ confirmacion = confirm('queres hacer otra operacion?')
 
-
-}
-
-if (isNaN(num2))  {
-
-    num2 = 0;
-
-}
-
-console.log(num1)
-console.log(num2)
-console.log(operacion)
-
-switch (operacion) {
-    case '+':
-        console.log(`El resultado de ${num1} + ${num2} = ${num1 + num2}`); 
-        
-        break;
-        case '-':
-        console.log(`El resultado de  ${num1} - ${num2} + ${num1 - num2}`)
-        break;
-        case '*': 
-        console.log(`El resultado de  ${num1} * ${num2} * ${num1 * num2}`)
-        break;
-        case '/': 
-        console.log(`El resultado de  ${num1} / ${num2} / ${num1 / num2}`)
-        break; 
-    default:
-
-        console.error(`La operacion ${operacion} no existe en esta calculadora`)
-        break;
-}
+} while (confirmacion);
