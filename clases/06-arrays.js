a9e2770cb4d2a0999b7c9552d60537ae4a5780b1
Oscar Mamani <omamani58@gmail.com>
2023-11-18 17:33:05 -0300

    clase07-arrays

--- clases/06-arrays.js
+++ clases/06-arrays.js
@@ -14,28 +14,64 @@
 //     }
 // }
 
-let listadoNombres = []
-let nombre
-do {
-     nombre = prompt('ingrese un nombre');
+// let listadoNombres = []
+// let nombre
+// do {
+//      nombre = prompt('ingrese un nombre');
    
-if (nombre) {
-    listadoNombres.push(nombre)
-}
+// if (nombre) {
+//     listadoNombres.push(nombre)
+// }
     
-} while (nombre);
+// } while (nombre);
 
 // console.log(listadoNombres)
 //para reemplazar
 // listadoNombres[0] = 'UNO'
-listadoNombres.splice(0, 3, 'UNO')
+// listadoNombres.splice(0, 3, 'UNO')
 
-for (let i = 0; i < listadoNombres.length; i++) {
-    // console.log(listadoNombres[i]);
-    console.log(`en la posicion ${i} esta el elemento: ${listadoNombres[i]}`);
-}
-
-
+// for (let i = 0; i < listadoNombres.length; i++) {
+//     // console.log(listadoNombres[i]);
+//     console.log(`en la posicion ${i} esta el elemento: ${listadoNombres[i]}`);
+// }
 
 
-document.write('hola mundo')
+
+
+// document.write('hola mundo')
+
+
+const alumnos = ['oscar', 'eduardo', 'pablo'];
+
+//agregamos un elemento al final
+alumnos.push('german','ivan');
+//agregamos un elemento al princio
+alumnos.unshift('guaso');
+//eliminar un elemento (ultimo)
+// alumnos.pop()
+//eliminar el primero
+// alumnos.shift()
+//MOSTRAR el ultimo elemento en un array
+console.log(alumnos.at(-1))
+console.log(alumnos.at(1))
+console.log(alumnos[alumnos.length - 1])
+
+
+//la posicion / index de un elemento
+let pos = alumnos.indexOf('oscar')
+
+console.log(pos)
+//saber si un elemnto existe en un arrays
+console.log(alumnos.includes('oscar'))
+
+const profes = ['nico', 'enrico']
+const comision87i = alumnos.concat(profes);
+
+console.log(alumnos)
+console.log(comision87i)
+console.log(comision87i.sort())
+console.log(comision87i.reverse())
+
+
+
+
